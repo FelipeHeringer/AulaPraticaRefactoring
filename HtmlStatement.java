67ac4f236b1ff9dc2b2bf91b92f9dc5ada97355a
Felipe Heringer <heringerfelipe14@gmail.com>
2025-11-23 22:17:57 -0300

    refactor: mover metodo value para a superclasse e manter Override nas subclasses

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,19 +1,4 @@
-import java.util.Enumeration;
-
 public class HtmlStatement extends Statement {
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = headerLine(aCustomer);
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            // show figures for each rental
-            result += showFigures(each);
-        }
-        // add footer lines
-        result += footerLines(aCustomer);
-        return result;
-    }
-
     @Override
     protected String headerLine(Customer aCustomer) {
         return "<H1>Rentals for <EM>" + aCustomer.getName() +
--- Statement.java
+++ Statement.java
@@ -1,5 +1,16 @@
+import java.util.Enumeration;
+
 public abstract class Statement {
-    public abstract String value(Customer aCustomer);
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = headerLine(aCustomer);
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += showFigures(each);
+        }
+        result += footerLines(aCustomer);
+        return result;
+    }
 
     protected abstract String headerLine(Customer aCustomer);
 
