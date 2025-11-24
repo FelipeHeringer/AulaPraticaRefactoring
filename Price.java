fc67d5e72776e44f838843a5861040007bef90b6
Felipe Heringer <heringerfelipe14@gmail.com>
2025-11-23 21:43:06 -0300

    refactor: decompor metodo getFrequentRenterPoints

--- Price.java
+++ Price.java
@@ -3,16 +3,7 @@ public abstract class Price {
 
     public abstract double getCharge(int daysRented);
 
-   protected int getFrequentRenterPoints(int _daysRented) {
-        // add frequent renter points
-        int frequentRenterPoints = 0;
-        frequentRenterPoints++;
-
-        // add bonus for a two day new release rental
-        if ((this.getPriceCode() == Movie.NEW_RELEASE) &&
-                _daysRented > 1)
-            return frequentRenterPoints++;
-
-        return frequentRenterPoints;
-   }
+    protected int getFrequentRenterPoints(int _daysRented) {
+        return 1;
+    }
 }
