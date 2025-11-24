81d7bc807c5bfb17a1ef1b56e7059fdae41834a1
Felipe Heringer <heringerfelipe14@gmail.com>
2025-11-23 21:37:54 -0300

    refactor: criar metodos especificos nas subclasses de Price

--- Price.java
+++ Price.java
@@ -1,24 +1,5 @@
 public abstract class Price {
    public abstract int getPriceCode();
-
-   protected double getCharge(int _daysRented){
-    double thisAmount = 0;
-        // determine amounts for each line
-        switch (this.getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (_daysRented > 2)
-                    thisAmount += (_daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += _daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (_daysRented > 3)
-                    thisAmount += (_daysRented - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
-   }
+   
+   public abstract double getCharge(int daysRented);
 }
