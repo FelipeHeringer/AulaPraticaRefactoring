d5ae9671c87a56dd1ab6ba52c3b3da4247797b5a
Felipe Heringer <heringerfelipe14@gmail.com>
2025-11-23 21:40:45 -0300

    refactor: mover metodo getFrequentRenterPoints para classe Price

--- Price.java
+++ Price.java
@@ -1,5 +1,18 @@
 public abstract class Price {
-   public abstract int getPriceCode();
-   
-   public abstract double getCharge(int daysRented);
+    public abstract int getPriceCode();
+
+    public abstract double getCharge(int daysRented);
+
+   protected int getFrequentRenterPoints(int _daysRented) {
+        // add frequent renter points
+        int frequentRenterPoints = 0;
+        frequentRenterPoints++;
+
+        // add bonus for a two day new release rental
+        if ((this.getPriceCode() == Movie.NEW_RELEASE) &&
+                _daysRented > 1)
+            return frequentRenterPoints++;
+
+        return frequentRenterPoints;
+   }
 }
--- entity/Movie.java
+++ entity/Movie.java
@@ -42,15 +42,7 @@ public class Movie {
     }
 
     public int getFrequentRenterPoints(int _daysRented) {
-        // add frequent renter points
-        int frequentRenterPoints = 0;
-        frequentRenterPoints++;
 
-        // add bonus for a two day new release rental
-        if ((this.getPriceCode() == Movie.NEW_RELEASE) &&
-                _daysRented > 1)
-            return frequentRenterPoints++;
-
-        return frequentRenterPoints;
+        return _price.getFrequentRenterPoints(_daysRented);
     }
 }
