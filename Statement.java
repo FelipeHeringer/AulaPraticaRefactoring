9965e9d55777cca6e31dde159bd9c480e7939f5e
Felipe Heringer <heringerfelipe14@gmail.com>
2025-11-23 22:13:16 -0300

    refactor: criar metodos abstratos na superclass Statement, Override nas subclasses

--- Statement.java
+++ Statement.java
@@ -1,3 +1,9 @@
-public class Statement {
-    
+public abstract class Statement {
+    public abstract String value(Customer aCustomer);
+
+    protected abstract String headerLine(Customer aCustomer);
+
+    protected abstract String showFigures(Rental aRental);
+
+    protected abstract String footerLines(Customer aCustomer);
 }
--- TextStatement.java
+++ TextStatement.java
@@ -3,20 +3,45 @@ import java.util.Enumeration;
 public class TextStatement extends Statement {
     public String value(Customer aCustomer) {
         Enumeration rentals = aCustomer.getRentals();
-        String result = "Rental Record for " + aCustomer.getName() +
-                "\n";
+
+        String result = headerLine(aCustomer);
+
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(each.getCharge()) + "\n";
+            result += showFigures(each);
         }
+
+        result += footerLines(aCustomer);
+        return result;
+    }
+
+    @Override
+    protected String headerLine(Customer aCustomer) {
+        return "Rental Record for " + aCustomer.getName() +
+                "\n";
+    }
+
+    @Override
+    protected String footerLines(Customer aCustomer) {
+        String result = "";
+
         // add footer lines
         result += "Amount owed is " +
                 String.valueOf(aCustomer.getTotalCharge()) + "\n";
         result += "You earned " +
                 String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
                 " frequent renter points";
+
         return result;
-    }   
+    }
+
+    @Override
+    protected String showFigures(Rental aRental) {
+        String result = "";
+        result += "\t" + aRental.getMovie().getTitle() + "\t" +
+                String.valueOf(aRental.getCharge()) + "\n";
+
+        return result;
+    }
 }
