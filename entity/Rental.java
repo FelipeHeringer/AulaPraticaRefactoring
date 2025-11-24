20bb5f2060fc56823ebe971c9c5fd6da26ca09f5
Felipe Heringer <heringerfelipe14@gmail.com>
2025-11-23 21:08:33 -0300

    refactor: extrair codigo para metodo getFrequentRenterPoints em Rental

--- entity/Rental.java
+++ entity/Rental.java
@@ -36,4 +36,17 @@ public class Rental {
         }
         return thisAmount;
     }
+
+    public int getFrequentRenterPoints() {
+        // add frequent renter points
+        int frequentRenterPoints = 0;
+        frequentRenterPoints++;
+
+        // add bonus for a two day new release rental
+        if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                this.getDaysRented() > 1)
+            return frequentRenterPoints++;
+
+        return frequentRenterPoints;
+    }
 }
