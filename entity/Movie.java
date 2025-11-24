ad18890dd4370f80603e38445f5d61ccaba21929
Felipe Heringer <heringerfelipe14@gmail.com>
2025-11-23 21:34:54 -0300

    refactor: mover metodo getCharge para classe Price

--- entity/Movie.java
+++ entity/Movie.java
@@ -37,24 +37,8 @@ public class Movie {
     }
 
     public double getCharge(int _daysRented) {
-        double thisAmount = 0;
-        // determine amounts for each line
-        switch (this.getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (_daysRented > 2)
-                    thisAmount += (_daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += _daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (_daysRented > 3)
-                    thisAmount += (_daysRented - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
+
+        return _price.getCharge(_daysRented);
     }
 
     public int getFrequentRenterPoints(int _daysRented) {
